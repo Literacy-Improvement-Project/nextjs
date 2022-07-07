3c7efd47664d431fc15a5d9326b87e5c219cb080
Jyeo <dhkfkr134@naver.com>
2022-07-08 00:27:25 +0900

    test

---
+++ store/modules/rootReducer.js
@@ -0,0 +1,25 @@
+import { combineReducers } from "@reduxjs/toolkit";
+import { HYDRATE } from "next-redux-wrapper";
+
+// import counter from './counterSlice';
+// import quizSlice from "./quizSlice";
+// import authSlice from "./authSlice";
+// import myOpenDictSlice from "./myOpenDictSlice";
+
+const rootReducer = (state, action) => {
+    if (action.type === HYDRATE) {
+        return {
+            ...state,
+            ...action.payload
+        };
+    }
+    return combineReducers({
+        // counter,
+        // quizSlice,
+        // authSlice,
+        // myOpenDictSlice,
+        // 여기에 추가 
+    })(state, action);
+}
+
+export default rootReducer;
