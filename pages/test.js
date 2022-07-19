0a896a8844ae035ebe1603d4ea3f52f38501072b
Jyeo <dhkfkr134@naver.com>
2022-07-19 22:19:45 +0900

    test

---
+++ pages/test.js
@@ -0,0 +1,13 @@
+
+import Seo from "../components/seo/Seo";
+
+export default function Test() {
+
+    return (
+        <div>
+            <Seo title="test" subtitle="test"></Seo>
+            <div>test</div>
+        </div>
+    )
+
+}
