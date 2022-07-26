834fea2b50d8ab23f4d88b16d4cc06fcae4845f8
Jyeo <dhkfkr134@naver.com>
2022-07-26 22:34:43 +0900

    add SearchWord component

---
+++ components/organism/SearchWordMeaning/AfterSearchWordMeaning.js
@@ -0,0 +1,23 @@
+import styles from './AfterSearchWordMeaning.module.css';
+import Meanbox from "../../molecule/meanbox/Meanbox";
+
+export default function AfterSearchWordMeaning({ sentences }) {
+
+    console.log(sentences)
+
+    return (
+        <div className={styles.container} >
+            {
+                sentences.map((item, index) => (
+                    <div key={index}>{
+                        item.morp.map((word) => (
+                            <div className={styles.sentences} key={word.id}>
+                                <Meanbox word={word.lemma} type={word.type}></Meanbox>
+                            </div>
+                        ))}
+                    </div>
+                ))
+            }
+        </div>
+    );
+}
