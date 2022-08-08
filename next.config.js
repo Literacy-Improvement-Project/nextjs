deb41510e2a8f3e2bb1287578c87487902f72abd
Cheolsoon <mn5724@naver.com>
2022-08-08 22:55:50 +0900

    fix login

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
-
+  
   async rewrites() {
 
     return [
       {
-        source: "/api/KakaoAuth",
+        source: "/api/kakaoAuth",
         destination: process.env.KAKAO_AUTH_URL,
       },
       {
